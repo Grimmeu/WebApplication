b0e3745417905cae531e4b3f50a5b39d43f7caa4
Kirill <grawerman@gmail.com>
2018-11-27 17:47:33 +0300

    WebApplication ->BookStore

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+    }
+}
